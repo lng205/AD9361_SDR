926333a1f17d99bfcd08105ffa850e5d6804abe3
lng205 <youbingyoubing@gmail.com>
2023-04-12 22:24:18 +0800

    temp

--- client.py
+++ client.py
@@ -1,6 +1,9 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.111.163', 9999))
-s.send(b'exit')
-s.close()
+import socket, serial
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ser = serial.Serial('/dev/ttyUSB2', baudrate=230400, rtscts=True)
+# while True:
+for i in range(10000):
+     d = ser.read()
+     print(ord(d))
+     s.sendto(d, ('192.168.10.122', 9999))
+# ser.close()
